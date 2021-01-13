2543c8955f59be1ab1bf914a7d7469ea1dccae2b
serkancam <sekocam@gmail.com>
2021-01-13 19:09:56 +0300

    Update image_read.py

--- opencv/image_read.py
+++ opencv/image_read.py
@@ -8,7 +8,6 @@ cd = os.getcwd()
 # marsrober.png resminin yolunu bulalım
 image_path = os.path.join(cd,"opencv","images","chp1","marsrover.png")
 # print("çalışılan dizin:",image_path)
-
 # resmi okuyalım
 image = cv2.imread(image_path)
 print(type(image),image.shape)
@@ -21,7 +20,6 @@ first_px_color = image[0,0]
 print(first_px_color)
 # ilk 200x100 pikseli yeşil yapalım
 image[0:100,0:200]=first_px_color#(0,255,0)
-
 # resim üstüne bir çizgi (line) çizelim
 start = (100,70)
 end = (350,380)
